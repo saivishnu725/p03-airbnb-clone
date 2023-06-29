8070ba05127a16aecdb073584d3c0074305bac18
saivishnu725 <saivishnu725@gmail.com>
2023-06-29 13:00:14 +0530

    added Card to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import NavBar from './components/NavBar.js';
 import Hero from "./components/Hero.js";
+import Card from "./components/Card.js";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Hero />
+      <Card />
     </div>
   );
 }
