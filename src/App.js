b67ed2dbfe9a9691d4bae70afd57f849faca98d6
saivishnu725 <saivishnu725@gmail.com>
2023-07-07 21:36:04 +0530

    used map() for the Cards

--- src/App.js
+++ src/App.js
@@ -2,43 +2,29 @@ import "./App.css";
 import NavBar from "./components/NavBar.js";
 import Hero from "./components/Hero.js";
 import Card from "./components/Card.js";
-import img1 from "./images/image-01.png";
-import img2 from "./images/image-02.png";
-import img3 from "./images/image-03.png";
+import CardData from "./components/CardData.js";
 
 function App() {
+  //image, rating, total_rating, location, title, price
+
+  const cards = CardData.map((c) => (
+    <Card
+      key={c.key}
+      image={c.image}
+      rating={c.rating}
+      total_rating={c.total_rating}
+      location={c.location}
+      title={c.title}
+      price={c.price}
+    />
+  ));
+
   return (
     <div className="App">
       <NavBar />
       <div className="body">
         <Hero />
-        <div className="cards">
-          {/* image, rating, total_rating, location, title, price */}
-          <Card
-            image={img1}
-            rating={5.0}
-            total_rating={6}
-            location="USA"
-            title="Life lessons with Katie Zaferes"
-            price={136}
-          />
-          <Card
-            image={img2}
-            rating={5.0}
-            total_rating={30}
-            location="USA"
-            title="Learn wedding photography"
-            price={125}
-          />
-          <Card
-            image={img3}
-            rating={4.8}
-            total_rating={2}
-            location="USA"
-            title="Group Mountain Biking"
-            price={50}
-          />
-        </div>
+        <div className="cards"> {cards} </div>
       </div>
     </div>
   );
