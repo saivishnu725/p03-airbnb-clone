104aea2adf21681abb7c2c5c5a5706e39e563954
saivishnu725 <saivishnu725@gmail.com>
2023-07-07 21:29:55 +0530

    created CardData file

---
+++ src/components/CardData.js
@@ -0,0 +1,32 @@
+import img1 from "./images/image-01.png";
+import img2 from "./images/image-02.png";
+import img3 from "./images/image-03.png";
+
+const a = [
+  {
+    image: img1,
+    rating: 5.0,
+    total_rating: 6,
+    location: "USA",
+    title: "Life lessons with Katie Zaferes",
+    price: 136,
+  },
+  {
+    image: img2,
+    rating: 5.0,
+    total_rating: 30,
+    location: "USA",
+    title: "Learn wedding photography",
+    price: 125,
+  },
+  {
+    image: img3,
+    rating: 4.8,
+    total_rating: 2,
+    location: "USA",
+    title: "Group Mountain Biking",
+    price: 50,
+  },
+];
+
+export default a;
