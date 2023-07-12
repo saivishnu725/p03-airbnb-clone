195ea92627564bf591e4d2556edbfd67fd77b1d5
saivishnu725 <saivishnu725@gmail.com>
2023-07-12 19:15:26 +0530

    object spreading or whatever

--- src/App.js
+++ src/App.js
@@ -10,12 +10,14 @@ function App() {
   const cards = CardData.map((c) => (
     <Card
       key={c.key}
-      image={c.image}
-      rating={c.rating}
-      total_rating={c.total_rating}
-      location={c.location}
-      title={c.title}
-      price={c.price}
+      // image={c.image}
+      // rating={c.rating}
+      // total_rating={c.total_rating}
+      // location={c.location}
+      // title={c.title}
+      // price={c.price}
+      // sold_out={c.sold_out}
+      {...c}
     />
   ));
 
